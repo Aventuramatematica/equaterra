c0a4a24535290531419e95e4cb84ed95b58c698f
Enzzo Hosaki <Enzzohosaki@hotmail.com>
2023-12-02 23:03:45 -0300

    Ajustes finos 3

--- Assets/Script/Porta.cs
+++ Assets/Script/Porta.cs
@@ -1,30 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Porta : MonoBehaviour
 {
-    [SerializeField]
-    private string nomeProximaFase;
+    // Nome da cena para a qual você deseja transicionar
+    public string nomeDaCena;
 
-    // Certifique-se de que o collider seja um "Is Trigger"
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        // Verificar se a colisão foi com o jogador (com base na camada)
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        // Verifica se o objeto que colidiu tem a tag "Player"
+        if (other.CompareTag("Player"))
         {
-            IrProxFase();
-            Debug.Log("Chamando próxima fase");
-        }
-    }
-
-    private void IrProxFase()
-    {
-        // Verificar se a cena que está sendo carregada é diferente da cena atual
-        if (SceneManager.GetActiveScene().name != this.nomeProximaFase)
-        {
-            SceneManager.LoadScene(this.nomeProximaFase);
+            // Transiciona para a cena desejada
+            SceneManager.LoadScene(nomeDaCena);
         }
     }
 }
+
